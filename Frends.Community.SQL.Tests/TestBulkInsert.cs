0b407d9e9a1e92c1dc1f35b46f7501a246b7a1c4
Janne Pennanen <jjj.pennanen@gmail.com>
2025-01-22 16:05:23 +0100

    Fixed tests to also use Microsoft.Data.SqlClient

--- Frends.Community.SQL.Tests/TestBulkInsert.cs
+++ Frends.Community.SQL.Tests/TestBulkInsert.cs
@@ -1,7 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Data;
 using System.Linq;
 using System.Threading;
